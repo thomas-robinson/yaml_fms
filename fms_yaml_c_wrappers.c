6e2b32e53d347a19a216446cef755c9a73b31c5e
Thomas Robinson <thomas.robinson@noaa.gov>
2021-06-09 11:28:16 -0400

    Adds lisence to fms_yaml_c_wrappers.c

--- fms_yaml_c_wrappers.c
+++ fms_yaml_c_wrappers.c
@@ -1,3 +1,32 @@
+/***********************************************************************
+ *                   GNU Lesser General Public License
+ *
+ * This file is part of the GFDL Flexible Modeling System (FMS).
+ *
+ * FMS is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * FMS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************/
+
+/**
+ * \file
+ * \author Tom Robinson
+ * \email gfdl.climate.model.info@noaa.gov 
+ *
+ * \section DESCRIPTION
+ *
+ * C wrappers for processing YAML using libfyaml
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -10,7 +39,7 @@
 /** \brief Parses yamlString to get the integer value of variable.
  * `variable` should have give the hierarchical structure of the key requested
  * in the form "/top/sub/level/key %d" with the %d to denote an integer.
- **/
+ */
 int fms_yaml_parse_int (char *yamlString, int yamlLen, char *variable) {
    struct fy_document *fyd;
    int count;
@@ -23,9 +52,9 @@ int fms_yaml_parse_int (char *yamlString, int yamlLen, char *variable) {
    return ivar;
 }
 /** \brief Parses yamlString to get the float value of variable.
- *  * `variable` should have give the hierarchical structure of the key requested
- *   * in the form "/top/sub/level/key %f" with the %f to denote a float.
- *    **/
+ * `variable` should have give the hierarchical structure of the key requested
+ * in the form "/top/sub/level/key %f" with the %f to denote a float.
+ **/
 float fms_yaml_parse_float (char *yamlString, int yamlLen, char *variable) {
    struct fy_document *fyd;
    int count;
@@ -39,9 +68,9 @@ float fms_yaml_parse_float (char *yamlString, int yamlLen, char *variable) {
 }
 
 /** \brief Parses yamlString to get the string value of variable.
- *  *  * `variable` should have give the hierarchical structure of the key requested
- *   *   * in the form "/top/sub/level/key %s" with the %s to denote a string.
- *    *    **/
+ * `variable` should have give the hierarchical structure of the key requested
+ * in the form "/top/sub/level/key %s" with the %s to denote a string.
+ **/
 char * fms_yaml_parse_string (char *yamlString, int yamlLen, char *variable) {
    struct fy_document *fyd;
    int count;
